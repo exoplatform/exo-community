a79f5931c39e1e98dde9f0ffd64c51a524bb5e8c
Ali Hamdi <ahamdi@exoplatform.com>
2022-06-22 18:11:15 +0100

    EXO-58026 : Add distribution type in platform.properties

--- platform-community-edition-service/src/main/java/org/exoplatform/platform/edition/EXOCommunityEdition.java
+++ platform-community-edition-service/src/main/java/org/exoplatform/platform/edition/EXOCommunityEdition.java
@@ -7,6 +7,6 @@ import org.exoplatform.container.ExoProfileExtension;
 public class EXOCommunityEdition implements ExoProfileExtension {
   @Override
   public Set<String> getProfiles() {
-    return Collections.singleton("exo-community");
+    return Collections.singleton("exo_community");
   }
 }
